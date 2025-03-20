7a74b2b343d2092b212678892e08f51a3655c3f3
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-20 12:15:34 +0100

    [UPD]Add-if-3

--- fizz.js
+++ fizz.js
@@ -2,4 +2,10 @@
 for(let numero = 1 ; numero <= 100 ; numero++){
  //CREO UNA VARIABILE CHE CONTERRA' LA STRINGA DA STAMPARE
  let stringa = "";
+
+    //SE IL NUMERO E' DIVISIBILE PER 3 AGGIUNGO Fizz
+    if(numero % 3 == 0){
+        // operatore di "addizione e assegnazione" che aggiunge il valore a destra al valore a sinistra e poi assegna il risultato alla variabile a sinistra
+        stringa += "Fizz";
+    }
 }
